72c85738f4faecff88ea2559d98fe6c6eebbd76c
Paul Horner <paul@mobilerocket.co.uk>
2018-11-06 15:21:29 +0000

    Ignore some updates based on attribute on model

--- src/UpdatedBy.php
+++ src/UpdatedBy.php
@@ -7,6 +7,24 @@ trait UpdatedBy
     {
         static::saving(function ($model)
         {
+            // we want to check to see if anything has actually changed on the model first
+            if (isset($model->ignore_updates) && $model->updated_by && $model->updated_by != 1) {
+                $changes = $model->getDirty();
+                $updated = [];
+                foreach ($changes as $key => $value) {
+                    if (!in_array($key, $model->ignore_updates)) {
+                        if ($original = $model->getOriginal($key)) {
+                            if ($original != $value) {
+                                $updated[$key] = $value;
+                            }
+                        }
+                    }
+                }
+                if (count($updated) == 0) {
+                    return;
+                }
+            }
+
             if(!\Auth::guest())
             {
                 $model->updated_by = \Auth::user()->id;
